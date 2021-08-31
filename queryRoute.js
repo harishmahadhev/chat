9489f1a6761eb979e0faa8a4dd7cc22751ac6df9
harishmahadhev <harishmahadhev@gmail.com>
2021-08-31 20:54:12 +0530

    added try catch block in async function

--- queryRoute.js
+++ queryRoute.js
@@ -3,13 +3,23 @@ const queryRouter = express.Router()
 const chatbot = require('./chatbot/chatbot.js')
 
 queryRouter.route("/df_event_query").post(async (req, res) => {
-    let result = await chatbot.eventQuery(req.body.event, req.body.parameters);
-    res.send(result);
+    try {
+        let result = await chatbot.eventQuery(req.body.event, req.body.parameters);
+        res.send(result);
+    } catch (error) {
+        res.send(error);
+    }
+
 })
 
 queryRouter.route("/df_text_query").post(async (req, res) => {
-    let result = await chatbot.textQuery(req.body.text, req.body.parameters);
-    res.send(result);
+    try {
+        let result = await chatbot.textQuery(req.body.text, req.body.parameters);
+        res.send(result);
+    } catch (error) {
+        res.send(error);
+    }
+
 })
 
 module.exports = queryRouter;
