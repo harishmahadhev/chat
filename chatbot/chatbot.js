ddf79ba95597c724e4e0df22399a50a5168906fa
harishmahadhev <harishmahadhev@gmail.com>
2021-08-31 21:23:32 +0530

    changes made

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -17,9 +17,7 @@ module.exports = {
             session: sessionPath,
             queryInput: {
                 text: {
-                    // The query to send to the dialogflow agent
                     text: text,
-                    // The language used by the client (en-US)
                     languageCode: config.dialogFlowSessionLanguageCode,
                 },
             },
