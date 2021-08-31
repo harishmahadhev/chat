75693562dc519d6ef0d37aae310fa48706abb75c
harishmahadhev <harishmahadhev@gmail.com>
2021-08-31 21:02:23 +0530

    ..

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.use(express.json())
 require('./routes/dialogFlowRoutes.js')(app)
 
 const PORT = process.env.PORT || 5000
+const node = process.env.NODE_ENV
+console.log(node)
 app.listen(PORT, () => {
     console.log("Server Started at PORT")
 })
