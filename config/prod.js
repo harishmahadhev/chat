cd477d3586397f6d67b7fe43752c0620f28ab3c9
harishmahadhev <harishmahadhev@gmail.com>
2021-09-01 16:04:50 +0530

    error from heroku has been fixed

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,7 @@
-const private_key = process.env.GOOGLE_PRIVATE_KEY;
-const googlePrivatekey = private_key.replace(/\\n/g, "\n");
 module.exports = {
     googleProjectID: process.env.GOOGLE_PROJECT_ID,
     dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
     dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUAGE_CODE,
     googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-    googlePrivateKey: googlePrivatekey
+    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY
 }
