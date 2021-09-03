9dfc56895060256cbc92269398f9eee6bac38d11
harishmahadhev <harishmahadhev@gmail.com>
2021-09-03 22:58:45 +0530

    ..test

--- database/db_connection.js
+++ database/db_connection.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 const { mongoURI } = require('../config/keys')
-mongoose.connect(mongoURI, { useNewUrlParser: true })
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.connection.on("open", () => console.log("MongoDb connected"))
--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -1,9 +1,6 @@
-// const fulfillmentRouter = require("./fulfillmentRoute")
+const fulfillmentRouter = require("./fulfillmentRoute")
 const queryRouter = require("./queryRoute")
 module.exports = app => {
-    app.get("/", (req, res) => {
-        res.send("HI")
-    })
+    app.use("/", fulfillmentRouter)
     app.use("/api", queryRouter)
-    // app.use("/", fulfillmentRouter)
 }
