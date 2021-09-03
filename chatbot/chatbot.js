359fdf96b191db631c5f5c4f84c300996230463f
harishmahadhev <harishmahadhev@gmail.com>
2021-09-04 00:15:04 +0530

    ..

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -36,7 +36,7 @@ module.exports = {
 
     eventQuery: async function (event, userId, parameters = {}) {
         const sessionPath = sessionClient.sessionPath(projectId, sessionId + userId)
-        console.log(sessionClient, projectId, credentials)
+
         let self = module.exports;
         const request = {
             session: sessionPath,
