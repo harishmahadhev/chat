0cc445131842618216c1055a50f0df54be5f7374
harishmahadhev <harishmahadhev@gmail.com>
2021-09-03 23:32:28 +0530

    ..

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
-
+const fulfillmentRouter = require("./routes/fulfillmentRoute")
+const queryRouter = require("./routes/queryRoute")
+require('./database/db_connection')
 const PORT = process.env.PORT || 5000
-
 app.use(express.json())
 app.use(cors())
-require('./database/db_connection')
-require('./routes/dialogFlowRoutes.js')(app)
+app.use("/api", queryRouter)
+app.use("/", fulfillmentRouter)
 
 
 app.listen(PORT, () => {
--- routes/dialogFlowRoutes.js
+++
@@ -1,7 +0,0 @@
-const fulfillmentRouter = require("./fulfillmentRoute")
-const queryRouter = require("./queryRoute")
-module.exports = app => {
-    app.use("/api", queryRouter)
-    app.use("/", fulfillmentRouter)
-
-}
