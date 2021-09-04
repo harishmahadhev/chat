fc71583dea01563c14d311ffc3c42c8654a82b9a
harishmahadhev <harishmahadhev@gmail.com>
2021-09-04 12:02:53 +0530

    added front end

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ app.use(cors())
 app.use("/api", queryRouter)
 app.use("/", fulfillmentRouter)
 
+if (process.env.NODE_ENV === 'production') {
+    // js and css files
+    app.use(express.static('client/build'));
+
+    // index.html for all page routes
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server Started at PORT ${PORT}`)
